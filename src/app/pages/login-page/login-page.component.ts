eda8efec3515fa56ac550fa8fd15250bd33b3e82
DiA <digo0909@gmail.com>
2024-03-27 15:47:16 -0500

    primera subida

---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+	selector: 'app-login-page',
+	templateUrl: './login-page.component.html',
+	styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent {
+	private readonly _router = inject(Router);
+
+	clickSingUp(): void {
+		this._router.navigate(['/home'], { queryParams: { user: 'jimy' }, state: { isAdmin: true } });
+	}
+}
